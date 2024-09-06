6ecb9c52668d96a151e6c8f78580c8db1e015dd0
NEXUS <publicbot34@gmail.com>
2024-09-05 23:41:45 -0500

    Create shopItem.js

---
+++ models/shopItem.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const shopItemSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    description: { type: String, default: '' },
+});
+
+module.exports = mongoose.model('ShopItem', shopItemSchema);
