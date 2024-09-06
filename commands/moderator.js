ec926e26c019eab8df65cdad86fe0f8f242ced36
NEXUS <publicbot34@gmail.com>
2024-09-05 20:12:12 -0500

    Update moderator.js

--- commands/moderator.js
+++ commands/moderator.js
@@ -1,5 +1,5 @@
+const { EmbedBuilder } = require('discord.js');
 const Moderator = require('../models/moderator');
-
 
 const PASSCODE = 'John';
 
@@ -8,29 +8,53 @@ module.exports = {
     description: 'Add a user as a moderator.',
     args: true,
     async execute(message, args) {
-        const isModerator = await Moderator.findOne({ userId: message.author.id });
-        const passcode = args.shift();
-        if (message.author.id !== process.env.CREATOR_ID && !isModerator && passcode !== PASSCODE) {
-            return message.reply('You do not have permission to use this command.');
+        try {
+            // Verify if the author is a moderator or has the correct passcode
+            const isModerator = await Moderator.findOne({ userId: message.author.id });
+            const passcode = args.shift();
+
+            if (message.author.id !== process.env.CREATOR_ID && !isModerator && passcode !== PASSCODE) {
+                return message.reply('🚫 **You don\'t have the power to use this command!** 🚫');
+            }
+
+            // Get the user to add
+            const userToAdd = message.mentions.users.first();
+            if (!userToAdd) {
+                return message.reply('🔍 **Please mention a user you want to promote to moderator!**');
+            }
+
+            // Check if the user is already a moderator
+            const existingMod = await Moderator.findOne({ userId: userToAdd.id });
+            if (existingMod) {
+                return message.reply(`🛡️ **${userToAdd.username} is already a moderator!**`);
+            }
+
+            // Add the user as a new moderator
+            const newModerator = new Moderator({
+                userId: userToAdd.id,
+                username: userToAdd.username
+            });
+
+            await newModerator.save();
+
+            // Create a fun and engaging embed message
+            const embed = new EmbedBuilder()
+                .setTitle('🎉 **New Moderator Unlocked!** 🎉')
+                .setDescription(`
+                🌟 **Congratulations!** 🌟
+                **${userToAdd.username}** has been promoted to moderator! 🎖️
+                They are now part of the team and will help keep everything in order. 👮‍♂️
+                Let’s give them a warm welcome! 🎊
+                `)
+                .setColor('#4CAF50') // Green color for success
+                .setFooter({ text: '©️ Nexus Inc. | Elevate Your Experience!' })
+                .setTimestamp();
+
+            await message.channel.send({ embeds: [embed] });
+
+        } catch (error) {
+            console.error('Error executing addmod command:', error);
+            message.reply('⚠️ **Oops! Something went wrong.** ⚠️');
         }
-
-        const userToAdd = message.mentions.users.first();
-        if (!userToAdd) {
-            return message.reply('Please mention a user to add as a moderator.');
-        }
-
-        // Add user as a moderator
-        const existingMod = await Moderator.findOne({ userId: userToAdd.id });
-        if (existingMod) {
-            return message.reply(`${userToAdd.username} is already a moderator.`);
-        }
-
-        const newModerator = new Moderator({
-            userId: userToAdd.id,
-            username: userToAdd.username
-        });
-
-        await newModerator.save();
-        message.channel.send(`${userToAdd.username} has been added as a moderator.`);
     },
 };
