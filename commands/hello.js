b1b7c01319faf8797b92bee2210794a9376d0c8f
NEXUS <publicbot34@gmail.com>
2024-09-05 20:10:56 -0500

    Update hello.js

--- commands/hello.js
+++ commands/hello.js
@@ -1,7 +1,18 @@
 module.exports = {
     name: 'hello',
-    description: 'Replies with a greeting!',
+    description: 'Replies with a random greeting!',
     execute(message, args) {
-        message.channel.send(`Hello, ${message.author.username}!`);
+        const greetings = [
+            `Hello, ${message.author.username}! Hope you're having an awesome day! 😄`,
+            `Hey there, ${message.author.username}! What's up? 😊`,
+            `Hi, ${message.author.username}! How's it going? 🌟`,
+            `Yo, ${message.author.username}! Ready to have some fun? 😎`,
+            `Greetings, ${message.author.username}! Wishing you a fantastic day! 🎉`
+        ];
+
+        // Select a random greeting
+        const randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
+
+        message.channel.send(randomGreeting);
     },
 };
