cdfe86a112f1ded0137bf12ae0cdc2e3efd48d2d
NEXUS <publicbot34@gmail.com>
2024-09-09 12:43:55 -0500

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,11 +1,10 @@
-// models/user.js
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     userId: { type: String, required: true, unique: true },
     lastDailyClaim: { type: Date, default: null },
     balance: { type: Number, default: 0 },
-    username: { type: String, required: true },
+    username: { type: String, default: null }, // Make username optional
     // Add other fields as needed
 });
 
