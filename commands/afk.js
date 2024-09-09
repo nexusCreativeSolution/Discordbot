612f852e2825c808e8207b0b84abd3d614df929b
NEXUS <publicbot34@gmail.com>
2024-09-09 11:48:13 -0500

    Update afk.js

--- commands/afk.js
+++ commands/afk.js
@@ -7,7 +7,7 @@ module.exports = {
     usage: '<reason>',
     async execute(message, args) {
         const reason = args.join(' ') || 'No reason provided 🌙';
-        
+
         // Save the user as AFK in MongoDB
         await Afk.findOneAndUpdate(
             { userId: message.author.id },
@@ -15,7 +15,7 @@ module.exports = {
             { upsert: true }
         );
 
-        // Fun and branded response
+        // Fun and branded response for setting AFK
         message.reply(`
 🚶‍♂️ *${message.author.username}* is now AFK! 💤
 Reason: ${reason}
@@ -25,4 +25,19 @@ In the meantime, maybe grab a snack? 🍕
     ©️Nexus Inc. – We’ll keep things running while you’re away! 😎
         `);
     },
+
+    async checkAfkStatus(message) {
+        const afkUser = await Afk.findOne({ userId: message.author.id });
+
+        if (afkUser) {
+            // Remove the user from AFK in MongoDB
+            await Afk.findOneAndDelete({ userId: message.author.id });
+
+            // Welcome them back
+            message.reply(`
+🎉 Welcome back, *${message.author.username}*! Hope you had a good break! 😄
+    ©️Nexus Inc. – We knew you'd return! 😎
+            `);
+        }
+    }
 };
