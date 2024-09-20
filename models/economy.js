4e996d4df78f5b50c47ef91323b7a6121c84d755
NEXUS <publicbot34@gmail.com>
2024-09-20 09:49:12 -0500

    Update economy.js

--- models/economy.js
+++ models/economy.js
@@ -4,6 +4,7 @@ const economySchema = new mongoose.Schema({
     userId: { type: String, required: true, unique: true },
     username: { type: String, required: true },
     balance: { type: Number, default: 0 },
+    lastWorkClaim: { type: Date, default: null }, // Add this field
 });
 
 module.exports = mongoose.model('Economy', economySchema);
